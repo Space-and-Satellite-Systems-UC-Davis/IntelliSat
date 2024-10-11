f5b02494de3d48ac8213efc010b13e200359770b
Anthony Surkov <anthonysurkov@gmail.com>
2024-10-10 18:36:21 -0700

    Started MB85RS256B.c, MB85RS256B.h. Updated TestDefinition.h. Tried updating spi.c, spi.h.

---
+++ Src/peripherals/FRAM/FRAM_testing.c
@@ -0,0 +1,13 @@
+/*
+ * FRAM_testing.c
+ *
+ *  Created on: Sep 3, 2024
+ *      Author: green
+ */
+
+#include "MB85RS256B.h"
+#include "print_scan.h"
+
+void testFunction_FRAM() {
+	fram_readDeviceID();
+}
---
+++ Src/peripherals/FRAM/MB85RS256B.h
@@ -0,0 +1,29 @@
+/*
+ * MB85RS256B.h
+ *
+ *  Created on: Sep 3, 2024
+ *      Author: green
+ */
+
+#ifndef PERIPHERALS_FRAM_MB85RS256B_H_
+#define PERIPHERALS_FRAM_MB85RS256B_H_
+
+#include "../../system_config/SPI/spi.h"
+#include "print_scan.h"
+
+//Registers
+#define FRAM_SPI            SPI1
+#define FRAM_SPI_CS         SPI1_CS
+
+//FRAM Op-codes
+#define FRAM_READ_ID        0x9F
+#define FRAM_READ_SR        0x05
+#define FRAM_WRITE_SR       0x01
+#define FRAM_WRITE_ENABLE   0x06
+#define FRAM_READ           0x03
+#define FRAM_WRITE          0x02
+
+//Explanation Here
+void fram_readDeviceID();
+
+#endif /* PERIPHERALS_FRAM_MB85RS256B_H_ */
