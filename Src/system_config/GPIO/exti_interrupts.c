35a89981735ce6df400a096836c613bbc39df985
FofLof <ethan_see@hotmail.com>
2024-10-28 20:32:34 -0700

    Working button SW1

--- Src/system_config/GPIO/exti_interrupts.c
+++ Src/system_config/GPIO/exti_interrupts.c
@@ -2,11 +2,11 @@
  * exti_interrupts.c
  *
  *  - September 23, 2023
- *  	Author 	: Darsh
- *  	Log		: changed to just contain EXTI interrupt handlers
+ *      Author  : Darsh
+ *      Log     : changed to just contain EXTI interrupt handlers
  *
- * 	- May 14-16, 2023
- * 		Author       : Raphael, Darsh, Parteek
+ *  - May 14-16, 2023
+ *      Author       : Raphael, Darsh, Parteek
  *      Contributors : nithilsenthil, Huey, Raymond, Kevin
  *      Log          : wrote the Button Handlers
  *
@@ -28,26 +28,29 @@
  * @returns None
  */
 void EXTI15_10_IRQHandler(){
-	/*
-	 * General Procedure
-	 * 		- Check which pins caused the interrupt
-	 * 		  by checking the Pending Request Register
-	 * 		- For the request that is pending, clear the interrupt flag
-	 * 		  by writing a 1 to the same Pending Request Register
-	 * 		- Call the Interrupt Handler for whatever
-	 * 		  is connected to that pin (or the pin itself)
-	 */
+    /*
+     * General Procedure
+     *      - Check which pins caused the interrupt
+     *        by checking the Pending Request Register
+     *      - For the request that is pending, clear the interrupt flag
+     *        by writing a 1 to the same Pending Request Register
+     *      - Call the Interrupt Handler for whatever
+     *        is connected to that pin (or the pin itself)
+     */
 
-	if (EXTI->PR1 & EXTI_PR1_PIF10) {		// Button 0
-		EXTI->PR1 |= EXTI_PR1_PIF10;
-#if OP_REV == 1 || OP_REV == 2
-		Button0_Handler();
-#endif
-	}
-	else if (EXTI->PR1 & EXTI_PR1_PIF11) {	// Button 1
-		EXTI->PR1 |= EXTI_PR1_PIF11;
-#if OP_REV == 1 || OP_REV == 2
-		Button1_Handler();
-#endif
-	}
+    #if OP_REV == 1 || OP_REV == 2
+        if (EXTI->PR1 & EXTI_PR1_PIF10) {       // Button 0
+            EXTI->PR1 |= EXTI_PR1_PIF10;
+            Button0_Handler();
+        } else if (EXTI->PR1 & EXTI_PR1_PIF11) {    // Button 1
+            EXTI->PR1 |= EXTI_PR1_PIF11;
+            Button1_Handler();
+        } 
+    #elif OP_REV == 3
+        if (EXTI->PR1 & EXTI_PR1_PIF12) {
+            EXTI->PR1 |= EXTI_PR1_PIF12;
+            ButtonSW1_Handler();
+        }
+    #endif
 }
+
