0b2148a7fe43f7f1b3568a7c36f15604bf1f587c
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-11-20 13:20:44 -0800

    Implemented FRAM read data function

--- Src/peripherals/FRAM/FRAM_testing.c
+++ Src/peripherals/FRAM/FRAM_testing.c
@@ -8,8 +8,8 @@
 #include "MB85RS256B.h"
 #include "print_scan.h"
 
-
-void FRAMtest_write_enable() {
+void FRAMtest_write_enable()
+{
 
 	uint8_t SR1 = FRAM_read_SR1();
 	printMsg("\n\rSR1 initial: %c", SR1);
@@ -18,21 +18,45 @@ void FRAMtest_write_enable() {
 
 	SR1 = FRAM_read_SR1();
 	printMsg("\n\rSR1 after WREn: %c", SR1);
-
 }
 
 // FRAM: Basic communications
 // Currently, bytes 1~3 are getting the correct 0x04 0x7F 0x05 code. byte 0 seems to get gibberish
 // Tried byte 5. Looks like that picks up the 0x09. Need to figure out why byte 0 is wasted currently
 // but otherwise, GOOD!
-void FRAMtest_read_deviceID() {
+void FRAMtest_read_deviceID()
+{
 	uint8_t MOSI[5];
 	FRAM_read_deviceID(MOSI);
-	for (uint8_t i = 0; i < 5; ++i) {
+	for (uint8_t i = 0; i < 5; ++i)
+	{
 		printMsg("\n%u", MOSI[i]);
 	}
 }
 
-void testFunction_FRAM() {
+void testFunction_FRAM()
+{
 	FRAMtest_read_deviceID();
+}
+
+void FRAMtest_readData()
+{
+	uint8_t buffer[256] = {0};
+	uint16_t address = 0x000000;
+
+	if (FRAM_readData(address, buffer))
+	{
+
+		printMsg("FRAM Data Read Successful:\n\r");
+		for (uint16_t i = 0; i < 256; ++i)
+		{
+			printMsg("0x%02X ", buffer[i]);
+			if ((i + 1) % 16 == 0)
+				printMsg("\n\r");
+		}
+	}
+	else
+	{
+		printMsg("FRAM Data Read Failed.\n\r");
+	}
 }
