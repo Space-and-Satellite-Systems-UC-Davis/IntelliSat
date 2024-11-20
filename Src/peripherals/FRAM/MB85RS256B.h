4d8b595b26c5df2845c63645ce2ce51266cf407f
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-11-20 13:20:44 -0800

    Implemented FRAM read data function

--- Src/peripherals/FRAM/MB85RS256B.h
+++ Src/peripherals/FRAM/MB85RS256B.h
@@ -11,20 +11,22 @@
 #include "../../system_config/SPI/spi.h"
 #include "print_scan.h"
 
-//Registers
-#define FRAM_SPI            SPI1
-#define FRAM_SPI_CS         SPI1_CS
+// Registers
+#define FRAM_SPI SPI1
+#define FRAM_SPI_CS SPI1_CS
 
-//FRAM Op-codes. SR = Status Register
-#define FRAM_READ_ID        0x9F
-#define FRAM_READ_SR        0x05
-#define FRAM_WRITE_SR       0x01
-#define FRAM_WRITE_ENABLE   0x06
-#define FRAM_READ           0x03
-#define FRAM_WRITE          0x02
+// FRAM Op-codes. SR = Status Register
+#define FRAM_READ_ID 0x9F
+#define FRAM_READ_SR 0x05
+#define FRAM_WRITE_SR 0x01
+#define FRAM_WRITE_ENABLE 0x06
+#define FRAM_READ 0x03
+#define FRAM_WRITE 0x02
+#define FRAM_MAX_BYTES 8000
 
-//Insert explanation here
-void FRAM_read_deviceID(uint8_t* MISO);
+// Insert explanation here
+void FRAM_read_deviceID(uint8_t *MISO);
 
+bool FRAM_readData(uint16_t address, uint8_t *buffer);
 
 #endif /* PERIPHERALS_FRAM_MB85RS256B_H_ */
