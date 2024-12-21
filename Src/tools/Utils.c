f306b0a0a9a260028fbe2c7eca5f2db143232ce7
HistidineDwarf <dvorakdwarf@gmail.com>
2024-12-21 14:14:51 -0800

    Wrapper declaration

---
+++ Src/tools/Utils.c
@@ -0,0 +1,13 @@
+// #include <globals.h>
+
+bool is_time_out(uint64_t start_time, uint64_t timeout_ms) {
+    return (WILL_LOOPS_TIMEOUT == true) && ((start_time + timeout_ms) < getSysTime());
+}
+
+void while_timeout(void (*do_work)(), bool (*is_done)(), uint64_t timeout_ms) {
+    uint64_t start_time = getSysTime(); //time in ms
+
+    while (is_done() == false && is_time_out(start_time, timeout_ms) == false) {
+        do_work();
+    }
+}
