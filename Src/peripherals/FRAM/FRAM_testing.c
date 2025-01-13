d5b3f89873e4f69ec675a34e3ae02ac5e6e33a58
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2025-01-13 12:39:59 -0800

    Fixing merge conflicts

--- Src/peripherals/FRAM/FRAM_testing.c
+++ Src/peripherals/FRAM/FRAM_testing.c
@@ -36,46 +36,10 @@ void FRAMtest_read_deviceID()
 
 void testFunction_FRAM()
 {
-	uint8_t MOSI[5];
-	uint16_t page = 2;
-	uint8_t buffer[256] = {0};
-	uint16_t address = 0x000000;
+    FRAMtest_readData();
+    FRAMtest_read_deviceID();
+    FRAMtest_readPage();
 
-	FRAM_read_deviceID(MOSI);
-	for (uint8_t i = 0; i < 5; ++i)
-	{
-		printMsg("\n%u", MOSI[i]);
-	}
-
-	if (FRAM_readData(address, buffer))
-	{
-		printMsg("FRAM Data Read Successful:\n\r");
-		for (uint16_t i = 0; i < 256; ++i)
-		{
-			printMsg("0x%02X ", buffer[i]);
-			if ((i + 1) % 16 == 0)
-				printMsg("\n\r");
-		}
-	}
-	else
-	{
-		printMsg("FRAM Data Read Failed.\n\r");
-	}
-
-	if (FRAM_readPage(page, buffer))
-	{
-		printMsg("FRAM Page Read Successful (Page %u):\n\r", page);
-		for (uint16_t i = 0; i < 256; ++i)
-		{
-			printMsg("0x%02X ", buffer[i]);
-			if ((i + 1) % 16 == 0)
-				printMsg("\n\r");
-		}
-	}
-	else
-	{
-		printMsg("FRAM Page Read Failed.\n\r");
-	}
 }
 
 void FRAMtest_readData()
