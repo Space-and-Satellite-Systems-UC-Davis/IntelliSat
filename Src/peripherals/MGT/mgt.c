3cb15b1625b3f6c8df5cc982ca7a82b6c032f1bf
Eric Xu <xydxydxyd1@gmail.com>
2025-01-22 16:24:31 -0800

    dev: remove unneeded things

--- Src/peripherals/MGT/mgt.c
+++ Src/peripherals/MGT/mgt.c
@@ -3,7 +3,7 @@
 *
 * Author:           Eric Xu  
 * Created:          11/03/24 
-* Description:      Based on ethanese's radio.c
+* Description:      Communicate to MGT
 *****************************************************************************/
 
 
@@ -16,14 +16,14 @@ void mgt_transmitBytes(uint8_t message[], int nbytes);
 const uint8_t PACKET_START = '{';
 const uint8_t PACKET_END = '}';
 const uint8_t ESCAPE = '\\';
-enum mgt_op {
-    SET_PERCENT = 'S',
-    GET_CURRENT = 'C',
-    ACKNOWLEDGE = 'A',
-    SHUTDOWN = 'D',
-    STOP_TIMER = 'T',
-};
-typedef enum mgt_op mgt_op;
+//enum mgt_op {
+//    SET_PERCENT = 'S',
+//    GET_CURRENT = 'C',
+//    ACKNOWLEDGE = 'A',
+//    SHUTDOWN = 'D',
+//    STOP_TIMER = 'T',
+//};
+//typedef enum mgt_op mgt_op;
 
 // Sequence number of the packet in transit
 bool seq_num = 0;
@@ -92,14 +92,14 @@ int mgt_getResponse(uint8_t* buf) {
 }
 
 /**
- * Retransmit request if timed out.
+ * Retransmit requests that timed out.
  */
 void mgt_retransmit() {
     mgt_updateResp();
     if (req_nbytes == 0) {
         return;
     }
-    if (!received_resp && getSysTime() - req_time > TIMEOUT) {
+    if (!received_resp && getSysTime() - req_time > MGT_ACKNOWLEDGEMENT_TIMEOUT) {
         req_time = getSysTime();
         mgt_transmitBytes(req_buf, req_nbytes);
     }
@@ -169,66 +169,6 @@ void mgt_transmitBytes(uint8_t message[], int nbytes) {
 }
 
 ///**
-// * TODO
-// * Pauses the system to wait for acknowledgement byte from other device
-// *
-// * @return false if time out, true if didn't received acknowledgement
-// */
-//bool mgt_waitForAcknowledgement(uint64_t initialTime) {
-//	while (mgt_readOneByte() != 'A') {
-//		if (getSysTime() - initialTime > MGT_ACKNOWLEDGEMENT_TIMEOUT) {
-//			return false;
-//		}
-//	}
-//	return true;
-//}
-//
-///**
-// * Send a packet to MGT and wait for acknowledgement
-// *
-// * @param op
-// * @param payload
-// * @param payload_nbytes
-// *
-// * @return bytes sent if message is acknowledged, -E_MGT_LOST if message is sent but not
-// * acknowledged, -E_MGT_INVALID if message is invalid and not sent
-// */
-//int mgt_transmitPacket(mgt_op op, uint8_t payload[], int payload_nbytes) {
-//    // Check for delimiters in payload
-//    for (int i = 0; i < payload_nbytes; i++) {
-//        if (payload[i] == '{' || payload[i] == '}')
-//            return -E_MGT_INVALID;
-//    }
-//
-//    uint8_t message[packet_maxnbytes];
-//    int i = 0;
-//    message[i++] = PACKET_START;
-//    message[i++] = op;
-//    memcpy(message + i, payload, payload_nbytes);
-//    i += payload_nbytes;
-//    message[i++] = PACKET_END;
-//	bool acknowledged = mgt_waitForAcknowledgement(getSysTime());
-//    return acknowledged? i : -E_MGT_LOST;
-//}
-//
-///**
-// * Receive a packet from MGT and put it into `op` and `payload`. If the packet
-// * payload overflows `payload_maxsz`, then flush the rest of the packet and
-// * return -E_MGT_OVERFLOW
-// */
-//int mgt_receivePacket(mgt_op* op, uint8_t* payload, int payload_maxsz) {
-//    int received_nbytes = 0;
-//    char buf;
-//    while (received_nbytes < payload_maxsz) {
-//        int bytes_read = usart_recieveBytes(MGT_USART, &buf, 1);
-//    }
-//    //if (usart_recieveBufferNotEmpty(MGT_USART))
-//    //    led_d2(true);
-//    //else
-//    //    led_d2(false);
-//}
-//
-///**
 // * Set percent to Coil X PWM 0/1
 // *
 // * @param coilNumber - A number between 0 and 2 (inclusive)
@@ -251,143 +191,3 @@ void mgt_transmitBytes(uint8_t message[], int nbytes) {
 //    if (ret < 0)
 //        return ret;
 //}
-//
-///**
-// * Send a request to transfer data to the ground station
-// *
-// * @param numBytesToSend: The number of bytes to send to the ground station
-// * @param numTimesToSend: Number of times to resend the data to ground station
-// *
-// * @return true if it succeeded and false if it failed
-// */
-////bool mgt_transferToGroundStationRequest(uint8_t numBytesToSend, int numTimesToSend) {
-////	mgt_sendByte('t');
-////	mgt_sendByte(numBytesToSend);
-////	mgt_sendByte(numTimesToSend);
-////
-////	return mgt_waitForAcknowledgement(getSysTime());
-////}
-//
-///**
-// * Gets the current state of the mgt as r = RX_ACTIVE, t = TX_ACTIVE, o = OFF
-// * If timesout will return -1 as an error
-// *
-// * @return the state of the mgt as a character
-// */
-////char mgt_receiveStateRequest(void){
-////	mgt_sendByte('R');
-////
-////	char byteRead = -1;
-////
-////	uint64_t initRequestTime = getSysTime();
-////	while (byteRead != 'r' && byteRead != 't' && byteRead != 'o') {
-////
-////		if (getSysTime() - initRequestTime > MGT_ACKNOWLEDGEMENT_TIMEOUT) {
-////			return -1;
-////		}
-////
-////		byteRead = mgt_readOneByte();
-////	}
-////
-////	mgt_sendByte('A');
-////
-////	return byteRead;
-////}
-//
-///**
-// * Send a stream of bytes
-// *
-// * @param numberOfBytes: Number of bytes that is being sent in the buffer
-// * @param buffer: the data to send
-// *
-// */
-////void mgt_sendByteStream(int numberOfBytes, uint8_t buffer[]) {
-////	uint64_t startingStreamTime = getSysTime();
-////	for (int i = 0; i < numberOfBytes; i++) {
-////			//If byte per second past threshold then halt byte sending
-////			if (i / ((startingStreamTime - getSysTime()) / 1000) > BYTE_PER_SECOND_LIMIT) {
-////				i--;
-////			} else {
-////				mgt_sendByte(buffer[i]);
-////			}
-////	}
-////}
-//
-//
-///**
-// * Sends a stream of bytes
-// *
-// * @param numberOfBytes: Number of bytes to send
-// * @param buffer: The buffer with the bytes to send
-// *
-// * @return False if sending failed and true otherwise
-// */
-////bool mgt_sendByteStreamToMemRequest(int numberOfBytes, uint8_t buffer[]) {
-////	uint64_t startingStreamTime = getSysTime();
-////	if (!mgt_sendTransferToMemRequest(numberOfBytes)) {
-////		return false;
-////	}
-////	mgt_sendByteStream(numberOfBytes, buffer);
-////	return true;
-////}
-////
-////void mgt_sendState(enum MgtState state) {
-////	if (getSysTime() - lastStateChange > MGT_MAX_STATE_CHANGE_TIME_LIMIT) {
-////		switch (state) {
-////			case MGT_OFF:
-////				mgt_sendByte('o');
-////				break;
-////			case MGT_TX_ACTIVE:
-////				mgt_sendByte('t');
-////				break;
-////			case MGT_RX_ACTIVE:
-////				mgt_sendByte('r');
-////				break;
-////		}
-////		lastStateChange = getSysTime();
-////	}
-////
-////}
-//
-//
-///**
-// * Reads bytes from the mgt into a buffer
-// *
-// * @param buffer: the buffer that will be filled by the mgt's data
-// * @param receive_buffer_size: the number of bytes the buffer will take
-// */
-//int mgt_readBytes(uint8_t buffer[], int receive_buffer_size) {
-//	uint16_t amount_to_recieve = receive_buffer_size;
-//	uint16_t amount_read = 0;
-//
-//	uint64_t initTime  = getSysTime();
-//	while (amount_read < amount_to_recieve) {
-//
-//			if (getSysTime() - initTime > 10) {
-//                break;
-//			}
-//
-//			amount_read += usart_recieveBytes(MGT_USART,
-//					buffer,
-//					(receive_buffer_size - amount_read));
-//			buffer += amount_read;	// pointer arithmetic :)
-//
-//            // usart_recieverTimedOut not implemented
-//			//if (usart_recieverTimedOut(MGT_USART)) {
-//			//	break;
-//			//}
-//	}
-//    // Not sure what this line does
-//	//amount_read = usart_recieveBytes(MGT_USART, buffer, receive_buffer_size);
-//    return amount_read;
-//}
-//
-///**
-// * Reads one byte from the buffer and returns it
-// */
-////int mgt_readOneByte() {
-////	uint8_t buffer[1];
-////	mgt_readBytes(buffer, 1);
-////	return buffer[0];
-////
-////}
--- Src/peripherals/MGT/mgt.h
+++ Src/peripherals/MGT/mgt.h
@@ -14,15 +14,11 @@
 #include <globals.h>
 #include <stm32l476xx.h>
 #include <UART/uart.h>
-#include <string.h>
 #include <stdarg.h>
-#include <stdio.h>
 #include <Timers/timers.h>
 
 #define MGT_USART USART2
 #define MGT_BAUDRATE 9600
-#define MGT_MAX_STATE_CHANGE_TIME_LIMIT 10
-#define BYTE_PER_SECOND_LIMIT 1000
 #define MGT_ACKNOWLEDGEMENT_TIMEOUT 1000
 
 #define E_MGT_LOST 1
@@ -34,23 +30,10 @@
 #define OVERHEAD_MAXBYTES 3
 // Without escaping payload
 #define PACKET_MAXBYTES PAYLOAD_MAXBYTES + OVERHEAD_MAXBYTES
-#define TIMEOUT 500
 
 void mgt_init();
 bool mgt_request(uint8_t payload[], int nbytes);
 int mgt_getResponse(uint8_t* buf);
 void mgt_retransmit();
-
-/* TODO: For testing; remove on deploy <15-01-25, Eric Xu> */
-//void mgt_transmitBytes(uint8_t message[], int nbytes);
-//int mgt_readOneByte();
-//int mgt_readBytes(uint8_t buffer[], int receive_buffer_size);
-//void mgt_sendByteStream(int numberOfBytes, uint8_t buffer[]);
-//bool mgt_sendByteStreamToMemRequest(int numBytes, uint8_t buffer[]); //TODO: Test
-//void mgt_sendState(enum MgtState state);
-//bool mgt_sendTransferToMemRequest(int numBytes); //TODO: Test
-//char mgt_receiveStateRequest(void);
-
-//int mgt_setCoilPercent(uint8_t coilNumber, uint8_t pwm, uint8_t percentage);
 
 #endif /* PERIPHERALS_MGT_MGT_H_ */
