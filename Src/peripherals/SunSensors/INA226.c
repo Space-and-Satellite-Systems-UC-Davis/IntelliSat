7ee92def7ae5cc668fc0edb2fcdbf6b136ce5cb3
sayam24 <sennayama@gmail.com>
2025-02-05 19:42:31 -0800

    initial code of driver

--- Src/peripherals/SunSensors/INA226.c
+++ Src/peripherals/SunSensors/INA226.c
@@ -1,12 +1,118 @@
 #include "INA226.h"
 
 /*************************** Initialize the 4 sensors *************************/
-Sun_Sensor Sensor_0 = {0, Pan0_GPIO, Pan0_GPIO, Pan0_SDA_PIN, Pan0_SCL_PIN};
-Sun_Sensor Sensor_1 = {1, Pan1_GPIO, Pan1_GPIO, Pan1_SDA_PIN, Pan1_SCL_PIN};
-Sun_Sensor Sensor_2 = {2, Pan2_GPIO, Pan2_GPIO, Pan2_SDA_PIN, Pan2_SCL_PIN};
-Sun_Sensor Sensor_3 = {3, Pan3_GPIO, Pan3_GPIO, Pan3_SDA_PIN, Pan3_SCL_PIN};
+static Sun_Sensor sensor_0 = {0, PAN0_GPIO, PAN0_GPIO, PAN0_SDA_PIN, PAN0_SCL_PIN, MODE_POWERED_DOWN};
+static Sun_Sensor sensor_1 = {1, PAN1_GPIO, PAN1_GPIO, PAN1_SDA_PIN, PAN1_SCL_PIN, MODE_POWERED_DOWN};
+static Sun_Sensor sensor_2 = {2, PAN2_GPIO, PAN2_GPIO, PAN2_SDA_PIN, PAN2_SCL_PIN, MODE_POWERED_DOWN};
+static Sun_Sensor sensor_3 = {3, PAN3_GPIO, PAN3_GPIO, PAN3_SDA_PIN, PAN3_SCL_PIN, MODE_POWERED_DOWN};
+static Sun_Sensor* sensors[4] = {&sensor_0, &sensor_1, &sensor_2, &sensor_3};
 
 
 /*************************** Parameters *************************/
-//static uint16_t mag_magFullScale = 0;
-static Sun_Sensor *current_selected = Sensor_0;
+static int current_sensor = 0;
+static int current_lsb; //in microamps
+static int config;
+
+int ceil(float num){
+    return num - (int)num > 0 ? (int)num + 1: (int)num;
+}
+
+void sensor_init(int averages, int bus_time, int shunt_time, int mode, int rshunt, int max_current){
+    for(int i = 0; i<4; i++){
+        softi2c_init(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN);
+        sensors[i]->mode = mode;
+    }
+    sensor_config_all(averages, bus_time, shunt_time, mode, rshunt, max_current);
+}
+
+void sensor_config_init(int averages, int bus_time, int shunt_time, int mode, int rshunt, int max_current){
+    config = averages << 9 | bus_time << 6 | shunt_time << 3 | mode;
+    current_lsb = ceil(max_current / 32768.0 * MICRO); //2^15
+    int cal = ceil(0.00512 / (current_lsb/MICRO * rshunt/MILLI)); //round to nearest integer above
+    for(int i = 0; i<4; i++){
+        softi2c_writeReg(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN, SENSOR_ADDRESS, 0, config);
+        softi2c_writeReg(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN, SENSOR_ADDRESS, 5, cal);
+        softi2c_writeReg(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN, SENSOR_ADDRESS, 6, 1 << 3); //set conversion ready flag
+    }
+}
+
+void trigger(){
+    bool powered = sensors[current_sensor]->mode == MODE_POWERED_DOWN;
+
+    set_mode(MODE_TRIGGERED, false);
+    int ready = 0;
+    while(!ready)
+    {
+        ready = softi2c_readReg(sensors[current_sensor]->SCL_GPIO, sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 6);
+        ready = (ready >> 3) & 1;
+    }
+    if(powered)
+        set_mode(MODE_POWERED_DOWN, false);
+}
+
+float get_shunt_voltage(){
+    if(sensors[current_sensor]->mode != MODE_CONTINUOUS)
+    {
+        trigger();
+    }
+    int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO,sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0x01);
+    return output * .0025;
+}
+
+
+float get_bus_voltage(){
+    if(sensors[current_sensor]->mode != MODE_CONTINUOUS)
+    {
+        trigger();
+    }
+    int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO,sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0x02);
+    return output * 1.25/(double)MILLI; 
+}
+
+
+float get_power(){
+    if(sensors[current_sensor]->mode != MODE_CONTINUOUS)
+    {
+        trigger();
+    }
+    int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO,sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0x03);
+    return output * current_lsb/(double)MICRO * 25; 
+}
+
+
+float get_current(){
+    if(sensors[current_sensor]->mode != MODE_CONTINUOUS)
+    {
+        trigger();
+    }
+    int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO,sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0x04);
+    return output * current_lsb / (double)MICRO;
+}
+
+
+void set_mode(int mode, bool all){
+    int changed_config = config & 7 | mode;
+    if(all)
+    {
+        for(int i = 0; i < 4; i++){
+            softi2c_writeReg(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN, SENSOR_ADDRESS, 0, changed_config);
+            sensors[i]->mode = mode;
+        }
+    }else{
+        softi2c_writeReg(sensors[current_sensor]->SCL_GPIO, sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0, changed_config);
+        sensors[current_sensor]->mode = mode;
+    }
+}
+
+
+void select_sensor(int sensor){
+    if(sensor < 0 || sensor > 3)
+        sensor = 0;
+    else
+        current_sensor = sensor;
+}
+
+
+int get_id(){
+    return softi2c_readReg(sensor_0.SCL_GPIO, sensor_0.SCL_PIN, sensor_0.SDA_GPIO, sensor_0.SDA_PIN, SENSOR_ADDRESS, 0xff );
+}
--- Src/peripherals/SunSensors/INA226.h
+++ Src/peripherals/SunSensors/INA226.h
@@ -6,29 +6,156 @@
 
 #include <I2C/i2c.h>
 
+/*Struct for the gpio and pins of the sun sensors*/
 typedef struct
 {
-    /* data */
     int ID;
     GPIO_TypeDef* SDA_GPIO;
     GPIO_TypeDef* SCL_GPIO;
     int SDA_PIN;
     int SCL_PIN;
+    int mode;
 }Sun_Sensor;
 
+//Address
+#define SENSOR_ADDRESS 0x4B
+
 //Macros for the Sunsensor pins
-#define Pan0_GPIO GPIOG
-#define Pan0_SDA_PIN 14
-#define Pan0_SCL_PIN 13
-#define Pan1_GPIO GPIOF
-#define Pan1_SDA_PIN 11
-#define Pan1_SCL_PIN 2
-#define Pan2_GPIO GPIOG
-#define Pan2_SDA_PIN 0
-#define Pan2_SCL_PIN 1
-#define Pan3_GPIO GPIOG
-#define Pan3_SDA_PIN 4
-#define Pan3_SCL_PIN 5
+#define PAN0_GPIO GPIOG
+#define PAN0_SDA_PIN 14
+#define PAN0_SCL_PIN 13
+#define PAN1_GPIO GPIOF
+#define PAN1_SDA_PIN 11
+#define PAN1_SCL_PIN 2
+#define PAN2_GPIO GPIOG
+#define PAN2_SDA_PIN 0
+#define PAN2_SCL_PIN 1
+#define PAN3_GPIO GPIOG
+#define PAN3_SDA_PIN 4
+#define PAN3_SCL_PIN 5
+
+//number of averages
+#define AVERAGE_1   0
+#define AVERAGE_4   1
+#define AVERAGE_16  2
+#define AVERAGE_64  3
+#define AVERAGE_128 4
+#define AVERAGE_256 5
+#define AVERAGE_512 6
+
+//conversion times in microseconds
+#define CONVERT_140     0
+#define CONVERT_204     1
+#define CONVERT_332     2
+#define CONVERT_588     3
+#define CONVERT_1100    4
+#define CONVERT_2116    5
+#define CONVERT_4156    6
+#define CONVERT_8244    7
+
+//modes
+#define MODE_CONTINUOUS     7
+#define MODE_TRIGGERED      3 
+#define MODE_POWERED_DOWN   0 
+
+//number conversions
+#define MICRO 1000000
+#define MILLI 1000
 
 
 
+/**
+ * Initializes the sun sensor with I2c, and calls config function
+ *
+ * @param averages Number of samples to collect and average
+ * @param bus_time The conversion time for bus voltage measurements
+ * @param shunt_time The conversion time for shunt voltage measurements
+ * @param mode The mode of sensor (triggered, continuous, powered down)
+ * @param rshunt The value of the external shunt used in milliohms
+ * @param max_current The value of the maximum current
+ *
+ * @returns None
+ */
+void sensor_init(int averages, int bus_time, int shunt_time, int mode, int rshunt, int max_current);
+
+/**
+ * Initial configuration of the sun sensor's config register and calibration register
+ *
+ * @param averages Number of samples to collect and average
+ * @param bus_time The conversion time for bus voltage measurements
+ * @param shunt_time The conversion time for shunt voltage measurements
+ * @param mode The mode of sensor (triggered, continuous, powered down)
+ * @param rshunt The value of the external shunt used
+ * @param max_current The value of the maximum current
+ *
+ * @returns None
+ */
+void sensor_config_init(int averages, int bus_time, int shunt_time, int mode, int rshunt, int max_current);
+
+/**
+ * Retriggers voltage calculations if in triggered mode.
+ * 
+ * @returns None
+ */
+void trigger();
+
+/**
+ * Reads the shunt voltage from the sun sensor
+ *
+ * @returns Voltage in mV 
+ */
+float get_shunt_voltage();
+
+
+/**
+ * Reads the bus voltage from the sun sensor
+ *
+ * @returns Voltage in Volts
+ */
+float get_bus_voltage();
+
+/**
+ * Reads the power from the sun sensor
+ *
+ * @returns Wattage as a 16 bit integer
+ */
+float get_power();
+
+/**
+ * Reads the current from the sun sensor
+ *
+ * @returns Wattage as a 16 bit integer
+ */
+float get_current();
+
+/**
+ * Resets the mode of the sensors
+ * @param mode The mode of the sensor (triggered, continuous, powered down)
+ * @param all If true, set all of the sensors to the mode, else only set current sensor
+ * 
+ * @returns None
+ */
+void set_mode(int mode, bool all);
+
+/**
+ * Selects between sun sensors 0-3
+ * @param sensor The sensor to be selected
+ * 
+ * @returns None
+ */
+void select_sensor(int sensor);
+
+/**
+ * Get id of sun sensor
+ * 
+ * @returns ID
+ */
+int get_id();
+
+/**
+ * Helper function to round up calculations to nearest number
+ * @param num
+ * 
+ * @returns num rounded up
+ */
+int ceil(float num);
