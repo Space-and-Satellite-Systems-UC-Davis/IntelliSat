bcbfd2c45ca25b3b9de43d6f35394f46af7ceafa
Eric Xu <xydxydxyd1@gmail.com>
2025-02-13 23:55:06 -0800

    fix: remove legacy mgt code

--- Src/peripherals/MGT/mgt.h
+++ Src/peripherals/MGT/mgt.h
@@ -19,21 +19,7 @@
 
 #define MGT_USART USART2
 #define MGT_BAUDRATE 9600
-#define MGT_ACKNOWLEDGEMENT_TIMEOUT 1000
-
-#define E_MGT_LOST 1
-#define E_MGT_INVALID 2
-#define E_MGT_OVERFLOW 3
-
-#define PAYLOAD_MAXBYTES 125
-// PACKET_START, SEQ_NUM, PACKET_END
-#define OVERHEAD_MAXBYTES 3
-// Without escaping payload
-#define PACKET_MAXBYTES PAYLOAD_MAXBYTES + OVERHEAD_MAXBYTES
 
 void mgt_init();
-bool mgt_request(uint8_t payload[], int nbytes);
-int mgt_getResponse(uint8_t* buf);
-void mgt_retransmit();
 
 #endif /* PERIPHERALS_MGT_MGT_H_ */
