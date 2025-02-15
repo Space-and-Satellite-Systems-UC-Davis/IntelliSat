579319d3f6e3658e5b2d3bd27543afe42367349e
Ethan See <ethan_see@hotmail.com>
2025-02-14 18:16:52 -0800

    Untested finished implementation of ADC

--- Src/system_config/ADC/adc_sun_sensors.c
+++ Src/system_config/ADC/adc_sun_sensors.c
@@ -1,7 +1,66 @@
 #include "adc_sun_sensors.h"
 
+PANEL* panel0;
+PANEL* panel1;
+PANEL* panel2;
+PANEL* panel3;
+PANEL* panel4;
+PANEL* panel5;
+
+void adc_initializePanelConstants() {
+    panel0->adcDiode0 = ADC3;
+    panel0->adcDiode1 = ADC3;
+    panel0->diode0Channel = 6;
+    panel0->diode1Channel = 7;
+
+    panel1->adcDiode0 = ADC3;
+    panel1->adcDiode1 = ADC2;
+    panel1->diode0Channel = 12;
+    panel1->diode1Channel = 5;
+
+    panel2->adcDiode0 = ADC2;
+    panel2->adcDiode1 = ADC2;
+    panel2->diode0Channel = 6;
+    panel2->diode1Channel = 7;
+
+    panel3->adcDiode0 = ADC3;
+    panel3->adcDiode1 = ADC3;
+    panel3->diode0Channel = 10;
+    panel3->diode1Channel = 9;
+
+
+    panel4->adcDiode0 = ADC2;
+    panel4->adcDiode1 = ADC2;
+    panel4->diode0Channel = 1;
+    panel4->diode1Channel = 2;
+
+    panel5->adcDiode0 = ADC2;
+    panel5->adcDiode1 = ADC2;
+    panel5->diode0Channel = 3;
+    panel5->diode1Channel = 4;
+}
+
+bool adc1_isCalibrationDone() { return (ADC1->CR & ADC_CR_ADCAL) != 0; }
+bool adc2_isCalibrationDone() { return (ADC2->CR & ADC_CR_ADCAL) != 0; }
+bool adc3_isCalibrationDone() { return (ADC3->CR & ADC_CR_ADCAL) != 0; }
+
+bool adc_isEOCDownADC1() { return !(ADC1->ISR & ADC_ISR_EOC); }
+bool adc_isEOSDownADC1() { return !(ADC1->ISR & ADC_ISR_EOS); }
+
+bool adc_isEOCDownADC2() { return !(ADC2->ISR & ADC_ISR_EOC); }
+bool adc_isEOSDownADC2() { return !(ADC2->ISR & ADC_ISR_EOS); }
+
+bool adc_isEOCDownADC3() { return !(ADC3->ISR & ADC_ISR_EOC); }
+bool adc_isEOSDownADC3() { return !(ADC3->ISR & ADC_ISR_EOS); }
+
+bool adc_isADRDYNotResetADC1() { return (ADC1->ISR & ADC_ISR_ADRDY) == 0; }
+bool adc_isADRDYNotResetADC2() { return (ADC2->ISR & ADC_ISR_ADRDY) == 0; }
+bool adc_isADRDYNotResetADC3() { return (ADC3->ISR & ADC_ISR_ADRDY) == 0; }
+
+
+
 void ADC1_gpio_init() {
-    
+    // Currently not being used
 }
 
 void ADC2_gpio_init() {
@@ -14,6 +73,9 @@ void ADC2_gpio_init() {
      * Pan5-pd0 PC2 ADC1(2)3_IN3
      * Pan5-pc1 PC3 ADC1(2)3_IN4
      */
+
+
+
     
     RCC->APB2ENR |= RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOCEN;
 
@@ -23,6 +85,8 @@ void ADC2_gpio_init() {
     GPIOA->PUPDR &= GPIO_PUPDR_PUPD0_Msk | GPIO_PUPDR_PUPD1_Msk | GPIO_PUPDR_PUPD2_Msk;
     GPIOC->PUPDR &= GPIO_PUPDR_PUPD0_Msk | GPIO_PUPDR_PUPD1_Msk | GPIO_PUPDR_PUPD2_Msk | GPIO_PUPDR_PUPD3_Msk;
 
+    GPIOA->ASCR |= GPIO_ASCR_ASC0 | GPIO_ASCR_ASC1 | GPIO_ASCR_ASC2;
+    GPIOC->ASCR |= GPIO_ASCR_ASC0 | GPIO_ASCR_ASC1 | GPIO_ASCR_ASC2 | GPIO_ASCR_ASC3; 
 
 }
 
@@ -39,32 +103,148 @@ void ADC3_gpio_init() {
     GPIOF->PUPDR &= GPIO_PUPDR_PUPD3_Msk | GPIO_PUPDR_PUPD4_Msk | GPIO_PUPDR_PUPD9_Msk | GPIO_PUPDR_PUPD7_Msk | GPIO_PUPDR_PUPD6_Msk;
 }
 
-void ADC1_init() {
+uint8_t adc_calibrateADC(ADC_TypeDef* adc) {
+    adc->CR &= ~ADC_CR_DEEPPWD;//makes sure ADC isn't in deep power down mode
+	adc->CR |= ADC_CR_ADVREGEN; //enables ADC voltage regulator
+    adc->CR &= ~ADC_CR_ADEN; //Disable ADC
+	adc->CR &= ~ADC_CR_ADCALDIF; //Single-ended input
+    adc->CR |= ADC_CR_ADCAL; //Enable Calibration
+
+
+
+    //Wait for Calibration to Finish
+    switch ((int)adc) {
+        case (int)ADC1:
+            wait_with_timeout(adc1_isCalibrationDone, DEFAULT_TIMEOUT_MS);
+            break;
+        case (int)ADC2:
+            wait_with_timeout(adc2_isCalibrationDone, DEFAULT_TIMEOUT_MS);
+            break;
+        case (int)ADC3:
+            wait_with_timeout(adc3_isCalibrationDone, DEFAULT_TIMEOUT_MS);
+            break;
+        default:
+            break;
+    }
+
+    return adc->CALFACT | ADC_CALFACT_CALFACT_S;
+}
+
+void adc_init() {
+    adc_initializePanelConstants();
+    ADC1_init();
+    ADC2_init();
+    ADC3_init();
+}
+
+void adcx_initCommon(ADC_TypeDef* adc) {
     RCC->APB2ENR |= RCC_AHB2ENR_ADCEN;
+    adc_calibrateADC(adc);
+    RCC->CCIPR &= ~RCC_CCIPR_ADCSEL; //enables peripheral clock
+	RCC->CCIPR |= RCC_CCIPR_ADCSEL_SYSCLK; //Sets ADC clock to system clock
+    ADC123_COMMON->CCR |= (ADC_CCR_VBATEN);
+    adc->SQR1 &= ~( ADC_SQR1_L ); //Sets number of channels in the sequence of 1
+	adc->SQR1 &= ~(ADC_SQR1_SQ1); //Resets the sequence
+}
+
+void ADC1_init() {
+    ADC1_gpio_init();
+    adcx_initCommon(ADC1);
+    //Not being set because need to know what pins being used and ADC1 is currently not being used
+
 }
 
 void ADC2_init() {
-    RCC->APB2ENR |= RCC_AHB2ENR_ADCEN;
-    // Ensure ADC is disabled before configuring
-    if (ADC2->CR & ADC_CR_ADEN) {
-        ADC2->CR |= ADC_CR_ADDIS;
-        while (ADC2->CR & ADC_CR_ADEN);
-    }
+    ADC2_gpio_init();
+    adcx_initCommon(ADC2);
+    ADC2->SMPR1 = 0; 
 
-    ADC1->CFGR &= ~ADC_CFGR_RES; // Set resolution to 12-bit (00)
-    ADC1->CFGR &= ~ADC_CFGR_SCANDIR; // Disable scan mode (single-channel)
-
-    // Configure sampling time
-    ADC1->SMPR |= ADC_SMPR_SMP_1; // Set sampling time (12.5 ADC clock cycles)
-
-    // Select Channel 5 (PA0)
-    ADC1->CHSELR = ADC_CHSELR_CHSEL5; // Only enable Channel 5
-
-    // Enable ADC
-    ADC1->CR |= ADC_CR_ADEN;
-    while (!(ADC1->ISR & ADC_ISR_ADRDY)); // Wait for ADC to be ready
+    //Set sample rate to slowest rate (640.5) per conversion 
+    ADC2->SMPR1 |=  ADC_SMPR1_SMP1 | ADC_SMPR1_SMP2  | ADC_SMPR1_SMP3 | ADC_SMPR1_SMP4 | ADC_SMPR1_SMP5 | ADC_SMPR1_SMP6 | ADC_SMPR1_SMP7;
+    adc_enable(ADC2);
 }
 
 void ADC3_init() {
-    RCC->APB2ENR |= RCC_AHB2ENR_ADCEN;
+    ADC3_gpio_init();
+    adcx_initCommon(ADC3);
+
+    //Set sample rate to slowest rate (640.5) per conversion 
+    ADC3->SMPR1 = 0; 
+    ADC3->SMPR1 |=  ADC_SMPR1_SMP6 | ADC_SMPR1_SMP7  | ADC_SMPR1_SMP9;
+    ADC3->SMPR2 = 0;
+    ADC3->SMPR2 |= ADC_SMPR2_SMP10 | ADC_SMPR2_SMP12; 
+
+    adc_enable(ADC3);
+}
+
+void adc_enable(ADC_TypeDef* adc){
+	adc->ISR |= ADC_ISR_ADRDY; // Set before enabling ADC
+	adc->CR |= ADC_CR_ADEN; //Enables ADC
+
+    switch((int) adc) {
+        case (int) ADC1:
+            wait_with_timeout(adc_isADRDYNotResetADC1, DEFAULT_TIMEOUT_MS); //Waits until ADRDY is reset
+            break;
+        case (int) ADC2:
+            wait_with_timeout(adc_isADRDYNotResetADC2, DEFAULT_TIMEOUT_MS); //Waits until ADRDY is reset
+            break;
+        case (int) ADC3:
+            wait_with_timeout(adc_isADRDYNotResetADC3, DEFAULT_TIMEOUT_MS); //Waits until ADRDY is reset
+            break;
+    }
+}
+
+
+uint16_t adc_readVoltageFromDiode(PANEL* panel, int diodeNumber) {
+    ADC_TypeDef* adc = NULL;
+    int channel = -1;
+
+    if (diodeNumber == DIODE0) {
+        adc = panel->adcDiode0;
+        channel = panel->diode0Channel;
+    } else {
+        adc = panel->adcDiode1;
+        channel = panel->diode1Channel;
+    }
+
+
+    adc->CR &= ~ADC_CR_ADSTART;
+    adc->SQR1 |= channel << ADC_SQR1_SQ1_Pos; //Set the channel in sequence to be converted
+    
+    adc->CR  |=  ( ADC_CR_ADSTART ); //Start conversion
+
+	// Wait for the 'End Of Conversion' flag.
+    switch((int)adc) {
+        case (int) ADC1:
+            wait_with_timeout(adc_isEOCDownADC1, DEFAULT_TIMEOUT_MS);    
+            break;
+        case (int) ADC2:
+            wait_with_timeout(adc_isEOCDownADC2, DEFAULT_TIMEOUT_MS);    
+            break;
+        case (int) ADC3:
+            wait_with_timeout(adc_isEOCDownADC3, DEFAULT_TIMEOUT_MS);    
+            break;
+    }
+	// Read the converted value (this also clears the EOC flag).
+	uint16_t adc_val = adc->DR;
+
+	// Wait for the 'End Of Sequence' flag and clear it.
+	switch((int)adc) {
+        case (int) ADC1:
+            wait_with_timeout(adc_isEOSDownADC1, DEFAULT_TIMEOUT_MS);    
+            break;
+        case (int) ADC2:
+            wait_with_timeout(adc_isEOSDownADC2, DEFAULT_TIMEOUT_MS);    
+            break;
+        case (int) ADC3:
+            wait_with_timeout(adc_isEOSDownADC3, DEFAULT_TIMEOUT_MS);    
+            break;
+    }
+	adc->ISR |=  ( ADC_ISR_EOS );
+
+    adc->SQR1 &= ~(channel << ADC_SQR1_SQ1_Pos); //Remove the channel from the sequence
+
+	// Return the ADC value.
+	return adc_val;
+
 }
--- Src/system_config/ADC/adc_sun_sensors.h
+++ Src/system_config/ADC/adc_sun_sensors.h
@@ -1,7 +1,58 @@
 #pragma once
 
 #include <stm32l476xx.h>
+#include <globals.h>
+
+#define RCC_CCIPR_ADCSEL_SYSCLK (3 << RCC_CCIPR_ADCSEL_Pos)
+#define RCC_CCIPR_ADCSEL_PLLSAI1 (2 << RCC_CCIPR_ADCSEL_Pos)
+#define RCC_CCIPR_ADCSEL_PLLSAI2 (2 << RCC_CCIPR_ADCSEL_Pos)
+
+
+typedef struct PANEL_STRUCT {
+    ADC_TypeDef* adcDiode0;
+    ADC_TypeDef* adcDiode1;
+    int diode0Channel;
+    int diode1Channel;
+} PANEL;
+
+enum DIODE {DIODE0, DIODE1};
+
+
+/** Public Functions */
+
+uint16_t adc_readVoltageFromDiode(PANEL* panel, int diodeNumber);
+void adc_init();
+
+/** Public Functions */
+
+
+/** Private functions */
+
+void adc_initializePanelConstants();
+
+bool adc1_isCalibrationDone();
+bool adc2_isCalibrationDone(); 
+bool adc3_isCalibrationDone();
+
+bool adc_isEOCDownADC1();
+bool adc_isEOSDownADC1();
+
+bool adc_isEOCDownADC2();
+bool adc_isEOSDownADC2();
+
+bool adc_isEOCDownADC3();
+bool adc_isEOSDownADC3();
 
 void ADC1_gpio_init();
 void ADC2_gpio_init();
 void ADC3_gpio_init();
+
+uint8_t adc_calibrateADC(ADC_TypeDef* adc);
+
+void adcx_initCommon(ADC_TypeDef* adc);
+
+void ADC1_init();
+void ADC2_init();
+void ADC3_init();
+
+/** Private functions */
