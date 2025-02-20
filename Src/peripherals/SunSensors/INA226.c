2849fc3c2598cd8bea4eef3b45a7279e9f60a369
sayam24 <sennayama@gmail.com>
2025-02-19 16:11:56 -0800

    change arrays and stuff

--- Src/peripherals/SunSensors/INA226.c
+++ Src/peripherals/SunSensors/INA226.c
@@ -1,15 +1,7 @@
 #include "INA226.h"
 
-/*************************** Initialize the 4 sensors *************************/
-static Sun_Sensor sensor_0 = {0, PAN0_GPIO, PAN0_GPIO, PAN0_SDA_PIN, PAN0_SCL_PIN, MODE_POWERED_DOWN};
-static Sun_Sensor sensor_1 = {1, PAN1_GPIO, PAN1_GPIO, PAN1_SDA_PIN, PAN1_SCL_PIN, MODE_POWERED_DOWN};
-static Sun_Sensor sensor_2 = {2, PAN2_GPIO, PAN2_GPIO, PAN2_SDA_PIN, PAN2_SCL_PIN, MODE_POWERED_DOWN};
-static Sun_Sensor sensor_3 = {3, PAN3_GPIO, PAN3_GPIO, PAN3_SDA_PIN, PAN3_SCL_PIN, MODE_POWERED_DOWN};
-static Sun_Sensor* sensors[4] = {&sensor_0, &sensor_1, &sensor_2, &sensor_3};
-
-
 /*************************** Parameters *************************/
-static int current_sensor = 0;
+static Sun_Sensor current_sensor = {0, PAN0_GPIO, PAN0_GPIO, PAN0_SDA_PIN, PAN0_SCL_PIN, MODE_POWERED_DOWN};
 static int current_lsb; //in microamps
 static int config;
 
@@ -19,8 +11,8 @@ int celing(float num){
 
 void sensor_init(int averages, int bus_time, int shunt_time, int mode, int rshunt, int max_current){
     for(int i = 0; i<4; i++){
-        softi2c_init(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN);
-        sensors[i]->mode = mode;
+        softi2c_init(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN);
+        current_sensor.mode = mode;
     }
     sensor_config(averages, bus_time, shunt_time, mode, rshunt, max_current);
 }
@@ -30,23 +22,23 @@ void sensor_config(int averages, int bus_time, int shunt_time, int mode, int rsh
     current_lsb = celing(max_current / 32768.0 * MICRO); //2^15
     int cal = celing(0.00512 / (current_lsb/MICRO * rshunt/MILLI)); //round to nearest integer above
     for(int i = 0; i<4; i++){
-        softi2c_writeReg16(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN, SENSOR_ADDRESS, 0, config);
+        softi2c_writeReg16(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 0, config);
         softi2c_delay();
-        softi2c_writeReg16(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN, SENSOR_ADDRESS, 5, cal);
+        softi2c_writeReg16(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 5, cal);
         softi2c_delay();
-        softi2c_writeReg16(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN, SENSOR_ADDRESS, 6, 1 << 3); //set conversion ready flag
+        softi2c_writeReg16(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 6, 1 << 3); //set conversion ready flag
         softi2c_delay();
     }
 }
 
 void trigger(){
-    bool powered = sensors[current_sensor]->mode == MODE_POWERED_DOWN;
+    bool powered = current_sensor.mode == MODE_POWERED_DOWN;
 
     set_mode(MODE_TRIGGERED, false);
     int ready = 0;
     while(!ready)
     {
-        ready = softi2c_readReg(sensors[current_sensor]->SCL_GPIO, sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 6);
+        ready = softi2c_readReg(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 6);
         ready = (ready >> 3) & 1;
     }
     if(powered)
@@ -54,25 +46,25 @@ void trigger(){
 }
 
 float get_shunt_voltage(){
-    int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO,sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0x01);
+    int output = softi2c_readReg(current_sensor.SCL_GPIO,current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 0x01);
     return output * .0025;
 }
 
 
 float get_bus_voltage(){
-    int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO,sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0x02);
+    int output = softi2c_readReg(current_sensor.SCL_GPIO,current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 0x02);
     return output * 1.25/MILLI; 
 }
 
 
 float get_power(){
-    int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO,sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0x03);
+    int output = softi2c_readReg(current_sensor.SCL_GPIO,current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 0x03);
     return output * current_lsb/(double)MICRO * 25; 
 }
 
 
 float get_current(){
-    int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO,sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0x04);
+    int output = softi2c_readReg(current_sensor.SCL_GPIO,current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 0x04);
     return output * current_lsb / (double)MICRO;
 }
 
@@ -82,12 +74,12 @@ void set_mode(int mode, bool all){
     if(all)
     {
         for(int i = 0; i < 4; i++){
-            softi2c_writeReg(sensors[i]->SCL_GPIO, sensors[i]->SCL_PIN, sensors[i]->SDA_GPIO, sensors[i]->SDA_PIN, SENSOR_ADDRESS, 0, changed_config);
-            sensors[i]->mode = mode;
+            softi2c_writeReg(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 0, changed_config);
+            current_sensor.mode = mode;
         }
     }else{
-        softi2c_writeReg(sensors[current_sensor]->SCL_GPIO, sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0, changed_config);
-        sensors[current_sensor]->mode = mode;
+        softi2c_writeReg(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 0, changed_config);
+        current_sensor.mode = mode;
     }
 }
 
@@ -95,23 +87,48 @@ void set_mode(int mode, bool all){
 void select_sensor(int sensor){
     if(sensor < 0 || sensor > 3)
         sensor = 0;
-    else
-        current_sensor = sensor;
+    
+    current_sensor.ID = sensor;
+    switch (sensor) {
+        case 0: 
+            current_sensor.SCL_GPIO = PAN0_GPIO;
+            current_sensor.SCL_PIN = PAN0_SCL_PIN;
+            current_sensor.SDA_GPIO = PAN0_GPIO;
+            current_sensor.SDA_PIN = PAN0_SDA_PIN;
+            break;
+        case 1:
+            current_sensor.SCL_GPIO = PAN1_GPIO;
+            current_sensor.SCL_PIN = PAN1_SCL_PIN;
+            current_sensor.SDA_GPIO = PAN1_GPIO;
+            current_sensor.SDA_PIN = PAN1_SDA_PIN;
+            break;
+        case 2: 
+            current_sensor.SCL_GPIO = PAN2_GPIO;
+            current_sensor.SCL_PIN = PAN2_SCL_PIN;
+            current_sensor.SDA_GPIO = PAN2_GPIO;
+            current_sensor.SDA_PIN = PAN2_SDA_PIN;
+            break;
+        case 3: 
+            current_sensor.SCL_GPIO = PAN3_GPIO;
+            current_sensor.SCL_PIN = PAN3_SCL_PIN;
+            current_sensor.SDA_GPIO = PAN3_GPIO;
+            current_sensor.SDA_PIN = PAN3_SDA_PIN;
+            break;
+    }
 }
 
 
 int get_id(){
 
     int output = 0;
-    output = softi2c_readReg16(sensors[current_sensor]->SCL_GPIO, sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, 0x45, 0xfe);
-	/*int hex_value = 0x40;
+	int hex_value = 0x40;
 	while (hex_value < 0x50)
 	{
-		output = softi2c_readReg16(sensors[current_sensor]->SCL_GPIO, sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, hex_value, 0xfe);
+		output = softi2c_readReg16(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, hex_value, 0xfe);
 		if (output != 0)
 			break;
 		hex_value +=1;
-	}*/
-    //int output = softi2c_readReg(sensors[current_sensor]->SCL_GPIO, sensors[current_sensor]->SCL_PIN, sensors[current_sensor]->SDA_GPIO, sensors[current_sensor]->SDA_PIN, SENSOR_ADDRESS, 0xfe);
+	}
+    //int output = softi2c_readReg(current_sensor.SCL_GPIO, current_sensor.SCL_PIN, current_sensor.SDA_GPIO, current_sensor.SDA_PIN, SENSOR_ADDRESS, 0xfe);
     return output;
 }
--- Src/peripherals/SunSensors/sensor_tester.c
+++ Src/peripherals/SunSensors/sensor_tester.c
@@ -8,19 +8,14 @@ void testFunction_INA_Outputs()
     led_d2(true);
     //sensor_init(AVERAGE_1, CONVERT_1100, CONVERT_1100, MODE_CONTINUOUS, 68,10);
     int id = get_id();
-    float voltage = get_bus_voltage();
     select_sensor(1);
     id = get_id();
-    voltage = get_bus_voltage();
     select_sensor(2);
     id = get_id();
-    voltage = get_bus_voltage();
     select_sensor(3);
     id = get_id();
-    voltage = get_bus_voltage();
     select_sensor(0);
     id = get_id();
-    voltage = get_bus_voltage();
 
 }
 
