b8fb3737248a9251fa3951e3561586536d66f56d
Eric Xu <xydxydxyd1@gmail.com>
2025-02-26 14:52:55 -0800

    fix: rx buffer cleared too early

--- Src/system_config/UART/uart_tester.c
+++ Src/system_config/UART/uart_tester.c
@@ -41,50 +41,47 @@ static void init() {
     usart_init(bus2, baudrate);
 }
 
+static int read(USART_TypeDef* bus, uint8_t* buf) {
+    int len = 0;
+    while (usart_receiveBufferNotEmpty(bus)) {
+        usart_receiveBytes(bus, (uint8_t*)buf + len, 1);
+        len++;
+    }
+    buf[len] = '\0';
+    return len;
+}
+
 static bool run_test() {
     // Buffers for bus1 and bus2 respectively
-    char buf1[50];
+    uint8_t buf1[50];
     int buf1_len = 0;
-    char buf2[50];
+    uint8_t buf2[50];
     int buf2_len = 0;
 
     debugMsg("Beginning test");
     debugMsg("[bus1 transmit, bus2 receive]");
+
+    char *test_msg = "Hello, world!";
+    debugMsg("Transmitting \"%s\"...", test_msg);
+    wait(200);
     usart_flushrx(bus1);
     usart_flushrx(bus2);
-
-    strcpy(buf1, "Hello, world!");
-    buf1_len = strlen(buf1);
-    usart_transmitBytes(bus1, (uint8_t*)buf1, buf1_len);
-    wait(500);
-    // Check buf2 RX
-    while (usart_receiveBufferNotEmpty(bus2)) {
-        usart_receiveBytes(bus2, (uint8_t*)buf2 + buf2_len, 1);
-        buf2_len++;
-    }
-    buf2[buf2_len] = '\0';
-    if (strcmp(buf1, buf2) != 0) {
-        debugMsg("Buffer 2 RX is unexpected. See following message.");
-        debug_cmp(buf1, buf2);
-        return false;
-    }
-    // Check buf1 RX
-    buf1_len = 0;
-    while (usart_receiveBufferNotEmpty(bus1)) {
-        usart_receiveBytes(bus1, (uint8_t*)buf1 + buf1_len, 1);
-        buf1_len++;
-    }
+    usart_transmitBytes(bus1, (uint8_t*)test_msg, strlen(test_msg));
+    wait(200);
+    buf1_len = read(bus1, buf1);
     buf1[buf1_len] = '\0';
-    if (strcmp(buf1, "") != 0) {
-        debugMsg("Buffer 1 RX is unexpected. See following message.");
-        debug_cmp("", buf1);
-        return false;
-    }
+    buf2_len = read(bus2, buf2);
+    buf2[buf2_len] = '\0';
+    debugMsg("Checking buf1 ...");
+    if (!debug_cmp("", buf1)) return false;
+    debugMsg("Checking buf2...");
+    if (!debug_cmp(test_msg, buf2)) return false;
+
+    debugMsg("Test completed!");
     return true;
 }
 
 void testFunction_UART() {
-    debugMsg("Testing UART");
     init();
     run_test();
 }
