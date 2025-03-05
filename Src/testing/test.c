56442bdc1b7ac4429f10ed97c2fe496515042a60
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-03-05 14:46:01 -0800

    Add basic example test

--- Src/testing/test.c
+++ Src/testing/test.c
@@ -1,8 +1,20 @@
 #include "./unity.h"
 
+void test_basic_example_unity();
+
 // NOTE: These are UNIT tests not related to ADCS
 int run_unit_tests() {
-	UNITY_BEGIN();
+    UNITY_BEGIN();
 
-	return UNITY_END();
+    RUN_TEST(test_basic_example_unity);
+
+    return UNITY_END();
+}
+
+void test_basic_example_unity() {
+    TEST_ASSERT(1);
+
+    // Try to use type specific functions as much as possible, as they produce
+    // better debugging output!
+    TEST_ASSERT_EQUAL_INT(1, 2);
 }
