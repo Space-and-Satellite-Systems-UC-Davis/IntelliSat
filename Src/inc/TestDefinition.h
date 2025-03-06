45a8fd1ffca5b0f6f97949e316b99605c7b2f890
sayam24 <sennayama@gmail.com>
2025-03-05 17:00:36 -0800

    add to sunsensors.c

--- Src/inc/TestDefinition.h
+++ Src/inc/TestDefinition.h
@@ -11,6 +11,7 @@ extern void testFunction_LogTimer_Callback();
 extern void testerFunction_PWMGeneration();
 extern void testFunction_IMU_Dupe_Reads();
 extern void testFunction_Diode_Reads();
+extern void testFunction_INA_TMP_Outputs();
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 //                           TEST REGISTRATION
@@ -32,6 +33,8 @@ void (*getTestFunction(int test_id))(void) {
 			return testerFunction_PWMGeneration;
 		case 8:
 			return testFunction_IMU_Dupe_Reads;
+		case 11:
+			return testFunction_INA_TMP_Outputs;
 		case 12:
 			return testFunction_Diode_Reads;
 		default:
