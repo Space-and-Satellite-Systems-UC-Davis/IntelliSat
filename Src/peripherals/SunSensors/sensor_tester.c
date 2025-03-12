9c18d0e325fb569fdb730aa04c63348bab3ab67c
sayam24 <sennayama@gmail.com>
2025-03-12 15:17:11 -0700

    delete struct

--- Src/peripherals/SunSensors/sensor_tester.c
+++ Src/peripherals/SunSensors/sensor_tester.c
@@ -3,15 +3,16 @@
 
 void testFunction_INA_TMP_Outputs()
 {
-    sun_sensor_init();
     float bus_voltage = read_bus_voltage(PANEL0);
     float shunt_voltage = read_shunt_voltage(PANEL0);
     float power = read_power(PANEL0);
     float current = read_current(PANEL0);
     float temp = read_temp(PANEL0, TEMP0);
+    float voltage = sun_sensors_readVoltage(PANEL0, DIODE0);
     
     
     while (1) {
+        voltage = sun_sensors_readVoltage(PANEL0, DIODE1);
         bus_voltage = read_bus_voltage(PANEL0);
         shunt_voltage = read_shunt_voltage(PANEL0);
         power = read_power(PANEL0);
