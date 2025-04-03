11a06fc20aaedb37eb0961c6ac6990f3c48c6514
nand1b <182454553+nand1b@users.noreply.github.com>
2025-04-02 18:02:44 -0700

    Refactor hdd code to allow for better concurrent w

---
+++ Src/hdd/hdd_drive.c
@@ -0,0 +1,55 @@
+/*
+ * Any functions or code relevant to directly driving the
+ * rotation of the hdd
+ */
+
+#include <Timers/timers.h>
+#include <Timers/system_timer.c>
+#include <ADC/adc.h>
+#include <LED/led.h>
+#include <inttypes.h>
+
+
+void setEffectiveLowerDuty(const float PERCENT, const float MIN_DUTY, const float TIME_MS) {
+	uint64_t end_time = getSysTime() + TIME_MS;start_time
+	if (PERCENT >= 1) {
+
+	}
+
+	// to retain smooth behavior, period must decrease as percent decreases
+	// since there will be much more of the period to lose speed on
+	// 125ms off time is a fairly smooth, so we adjust the period to reach this
+	uint64_t ON_TIME;
+	uin64_t OFF_TIME;
+	while(getSysTime() < end_time) {
+
+	}
+
+}
+
+// default ramp
+void ramp(const float TARGET_DUTY, const float MIN_DUTY, const float MAX_DUTY);
+
+// reverse ramp
+void rampRev(const float TARGET_DUTY, const float MIN_DUTY, const float MAX_DUTY);
+
+void ramp(const float TARGET_DUTY, const float MIN_DUTY, const float MAX_DUTY){
+	printMsg("Starting Execution. \r\n");
+	const float MID_DUTY = (MAX_DUTY + MIN_DUTY) / 2;
+	const float DUTY_STEP = (MAX_DUTY - MID_DUTY) / 10;
+	const int DELAY_MS = 250;  // time between ramps
+
+	// start at the middle duty + some base speed, then slowly increase
+	const float INIT_DUTY = MID_DUTY + DUTY_STEP;
+	float current_duty = INIT_DUTY;
+	while (current_duty <= TARGET_DUTY && current_duty <= MAX_DUTY){
+		pwm_setDutyCycle(current_duty);
+		printMsg("Duty cycle: %f \r\n", current_duty);
+		current_duty += DUTY_STEP;
+		delay_ms(DELAY_MS);
+	}
+}
+
+void rampRev(const float TARGET_DUTY, const float MIN_DUTY, const float MAX_DUTY) {
+	ramp(TARGET_DUTY, MAX_DUTY, MIN_DUTY);
+}
--- Src/inc/TestDefinition.h
+++ Src/inc/TestDefinition.h
@@ -11,11 +11,34 @@ extern void testFunction_LogTimer_Callback();
 extern void testerFunction_PWMGeneration();
 extern void testFunction_IMU_Dupe_Reads();
 <<<<<<< HEAD
+<<<<<<< HEAD
 extern void testFunction_Diode_Reads();
 extern void testFunction_INA_TMP_Outputs();
 =======
+=======
+extern int testFuncID_HDD();
+>>>>>>> 927109c (Refactor hdd code to allow for better concurrent work.)
 extern void testFunction_HDD();
 >>>>>>> 5779b34 (Working PWM)
+
+// if your id is not present, add it
+extern void testFunction_HDD1();
+extern void testFunction_HDD2();
+extern void testFunction_HDD3();
+extern void testFunction_HDD4();
+extern void testFunction_HDD5();
+extern void testFunction_HDD6();
+
+// add your id's test function below
+static void (*testFunctions_HDD[6])() = {
+		testFunction_HDD,
+		testFunction_HDD1,
+		testFunction_HDD2,
+		testFunction_HDD3,
+		testFunction_HDD4,
+		testFunction_HDD5,
+		testFunction_HDD6
+};
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 //                           TEST REGISTRATION
@@ -44,8 +67,12 @@ void (*getTestFunction(int test_id))(void) {
 			return testFunction_Diode_Reads;
 =======
 		case 10:
+<<<<<<< HEAD
 			return testFunction_HDD;
 >>>>>>> 2912978 (Update testing definitions specifically for HDD.)
+=======
+			return testFunctions_HDD[testFuncID_HDD()];
+>>>>>>> 927109c (Refactor hdd code to allow for better concurrent work.)
 		default:
 			return testFunction_Nothing;
 	}
