303d966530a3b81439fcd7ca028c17fde504cb45
sayam24 <sennayama@gmail.com>
2025-04-08 20:02:53 -0700

    done testing pwm

--- Src/system_config/Timers/PWMTimerTest.c
+++ Src/system_config/Timers/PWMTimerTest.c
@@ -3,17 +3,35 @@
 #include <print_scan.h>
 
  void testerFunction_PWMGeneration() {
- 	pwm_initTimer(PWM0, 20000);
-        pwm_initTimer(PWM1, 20000);
-
  	while (1) {
-                pwm_timerOn(PWM0);
-                pwm_setDutyCycle(PWM0, 20);
-                delay_ms(10000);
-                pwm_timerOff(PWM0);
-                pwm_timerOn(PWM1);
-                pwm_setDutyCycle(PWM1, 20);
-                delay_ms(10000);
-                pwm_timerOff(PWM1);
+
+ 		for(int j = 5000; j<40000; j+=1000 ){
+ 		                        pwm_initTimer(PWM0, j);
+ 		                        pwm_initTimer(PWM1, j);
+ 		                        pwm_timerOn(PWM0);
+ 		                        pwm_setDutyCycle(PWM0, 30);
+ 		                        delay_ms(500);
+ 		                        pwm_timerOff(PWM0);
+ 		                        pwm_timerOn(PWM1);
+ 		                        pwm_setDutyCycle(PWM1, 30);
+ 		                        delay_ms(500);
+ 		                        pwm_timerOff(PWM1);
+ 		                        delay_ms(10);
+ 		                }
+                pwm_initTimer(PWM0, 20000);
+                pwm_initTimer(PWM1, 20000);
+                for(int i = 0; i<=100; i++){
+                        pwm_timerOn(PWM0);
+                        pwm_setDutyCycle(PWM0, i);
+                        delay_ms(1000);
+                        pwm_timerOff(PWM0);
+                        pwm_timerOn(PWM1);
+                        pwm_setDutyCycle(PWM1, i);
+                        delay_ms(1000);
+                        pwm_timerOff(PWM1);
+                        delay_ms(10);
+                }  
+
+                  
  	}
  }
