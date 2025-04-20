fba4ee773b4096e861ac68905aa6e1baa2de50fc
sayam24 <sennayama@gmail.com>
2025-04-20 11:34:23 -0700

    iwdg is complete

--- Src/system_config/WDG/watchdog.c
+++ Src/system_config/WDG/watchdog.c
@@ -5,7 +5,7 @@
 //timeout is 262144 cycles ~50ms
 void watchdog_config() {
 	IWDG->KR = IWDG_KICK; //PLEASE DO NOT REMOVE :) (Just deal with the consequences i guess if you really want to)
-	WWDG->CR |= WWDG_KICK;
+	//WWDG->CR |= WWDG_KICK;
 
     //if reset by watchdog or flash loading, don't need to reconfigure.
     if(RCC->CSR & RCC_CSR_OBLRSTF || RCC->CSR & RCC_CSR_IWDGRSTF || RCC->CSR & RCC_CSR_WWDGRSTF){
@@ -35,7 +35,7 @@ void watchdog_config() {
 
     //enable hardware watchdog for IWDG and WWDG
     FLASH->OPTR &= ~FLASH_OPTR_IWDG_SW_Msk;
-    FLASH->OPTR &= ~FLASH_OPTR_WWDG_SW_Msk;
+    //FLASH->OPTR &= ~FLASH_OPTR_WWDG_SW_Msk;
 
     //set option start bit in Flash control
     FLASH->CR |= FLASH_CR_OPTSTRT_Msk;
@@ -52,7 +52,7 @@ void watchdog_kick(){
 	int count = 0;
 	while(1){
 		IWDG->KR = IWDG_KICK;
-        WWDG->CR |= WWDG_KICK;
+        //WWDG->CR |= WWDG_KICK;
 		//printMsg("%d\n", count++);
         count++;
         if(count > 1000){
