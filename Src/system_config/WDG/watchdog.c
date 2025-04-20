b6cf3f9f14a870169591e9b1ba1a6cff169c1b7c
sayam24 <sennayama@gmail.com>
2025-04-20 12:39:32 -0700

    wwdg added

--- Src/system_config/WDG/watchdog.c
+++ Src/system_config/WDG/watchdog.c
@@ -5,11 +5,14 @@
 //timeout is 262144 cycles ~50ms
 void watchdog_config() {
 	IWDG->KR = IWDG_KICK; //PLEASE DO NOT REMOVE :) (Just deal with the consequences i guess if you really want to)
-	//WWDG->CR |= WWDG_KICK;
+	WWDG->CR |= WWDG_KICK;
 
+    if(!(FLASH->OPTR & (FLASH_OPTR_IWDG_SW |FLASH_OPTR_WWDG_SW))){
+        return;
+    }
     //if reset by watchdog or flash loading, don't need to reconfigure.
-    if(RCC->CSR & RCC_CSR_OBLRSTF || RCC->CSR & RCC_CSR_IWDGRSTF || RCC->CSR & RCC_CSR_WWDGRSTF){
-    	RCC->CSR |= RCC_CSR_RMVF;
+    printMsg("RCC IS: %x" ,RCC->CSR);
+    /*if(RCC->CSR & RCC_CSR_OBLRSTF || RCC->CSR & RCC_CSR_IWDGRSTF || RCC->CSR & RCC_CSR_WWDGRSTF){
         if(RCC->CSR & RCC_CSR_OBLRSTF){
             printMsg("Reset by option bytes! \n");
         } else if(RCC->CSR & RCC_CSR_IWDGRSTF){
@@ -17,8 +20,9 @@ void watchdog_config() {
         }else{
             printMsg("Reset by WWDG!\n");
         }
+    	//RCC->CSR |= RCC_CSR_RMVF;
         return;
-    }
+    }*/
     //enable hardware watchdog
 
     while(FLASH->SR & FLASH_SR_BSY_Msk);
@@ -35,7 +39,7 @@ void watchdog_config() {
 
     //enable hardware watchdog for IWDG and WWDG
     FLASH->OPTR &= ~FLASH_OPTR_IWDG_SW_Msk;
-    //FLASH->OPTR &= ~FLASH_OPTR_WWDG_SW_Msk;
+    FLASH->OPTR &= ~FLASH_OPTR_WWDG_SW_Msk;
 
     //set option start bit in Flash control
     FLASH->CR |= FLASH_CR_OPTSTRT_Msk;
@@ -52,10 +56,10 @@ void watchdog_kick(){
 	int count = 0;
 	while(1){
 		IWDG->KR = IWDG_KICK;
-        //WWDG->CR |= WWDG_KICK;
+        WWDG->CR |= WWDG_KICK;
 		//printMsg("%d\n", count++);
         count++;
-        if(count > 1000){
+        if(count > 1000000){
             printMsg("uh oh%d\n", count);
             int x = sun_sensors_readVoltage(PANEL0,DIODE0);
             printMsg("voltage: %f\n", x);
--- Src/system_config/WDG/watchdog_test.c
+++ Src/system_config/WDG/watchdog_test.c
@@ -5,6 +5,8 @@
 void testFunction_watchdog() {
     //int x = sun_sensors_readVoltage(PANEL0,DIODE0);
     //printMsg("voltage: %f\n", x);
+    //RCC->CSR |= RCC_CSR_RMVF;
+    WWDG->CR |= WWDG_KICK;
     printMsg("start watchdog\n");
     led_d2(true);
     watchdog_config();
