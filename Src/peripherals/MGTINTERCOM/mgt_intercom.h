91b070ec5e68d125e2a5101997372ec2ab5ae54d
James Knepper <jwknepper@ucdavis.edu>
2025-05-21 20:20:43 -0700

    Populate mgt_intercom.h

---
+++ Src/peripherals/MGTINTERCOM/mgt_intercom.h
@@ -0,0 +1,56 @@
+/**
+ * mgt_intercomm_intercom.h
+ * 
+ * Driver for communication with the magnetorquer
+ * 
+ * Author: James Knepper (2025)
+ * 
+ * Log:
+ * 21 May 2025 - Initial
+ */
+
+ #include <globals.h>
+ #include <UART/pcp.h>
+
+void mgt_intercomm_init();
+
+ /**
+  * Set the PWM for a coil
+  * 
+  * @param device  A pointer to the PCP device of the MGT
+  * @param coil_number  The number of the coil whose PWM is being set
+  * @param pwm  1 or 0, for pwm 1 or 0
+  * @param percent  The PWM percentage (0-100)
+  * 
+  * @returns  Success (1) or failure (0)
+  */
+ bool mgt_intercomm_set_coil_percent(PCPDevice * device, int coil_number, int pwm, int percentage);
+
+ /**
+  * Get the current through a coil
+  * 
+  * @param device  A pointer to the PCP device of the MGT
+  * @param coil_number  the number of the coil
+  * 
+  * @returns  the current in Amps (A)
+  */
+ float mgt_intercomm_get_current(PCPDevice * device, int coil_number);
+
+ /**
+  * Shut down all PWMs and timers on the MGT side
+  * 
+  * @param device  A pointer to the PCP device of the MGT
+  * 
+  * @returns  success (1) or failure (0)
+  */
+ bool mgt_intercomm_shutdown_all(PCPDevice * device);
+
+ /**
+  * Shut down a specific timer
+  * 
+  * @param device  A pointer to the PCP device of the MGT
+  * @param timer_number  The id of the timer to be turned off
+  * 
+  * @returns Success (1) or failure (0)
+  */
+ bool mgt_intercomm_shutdown_timer(PCPDevice * device, int timer_number);
