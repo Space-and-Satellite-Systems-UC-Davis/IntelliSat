7dcef9247743a4805aaf94271ebb64c1112f8ff2
HistidineDwarf <dvorakdwarf@gmail.com>
2025-05-27 18:55:38 -0700

    Simpler initialization and USART definitions

--- Src/system_config/DMA/peripheral_defs.c
+++ Src/system_config/DMA/peripheral_defs.c
@@ -9,8 +9,10 @@
 DMAPeripheral dma_adc1, dma_adc2, dma_adc3,
 	dma_spi1_rx, dma_spi1_tx,
 	dma_spi2_rx, dma_spi2_tx,
-	dma_spi3_rx, dma_spi3_tx;
-
+	dma_spi3_rx, dma_spi3_tx,
+	dma_usart1_rx, dma_usart1_tx,
+	dma_usart2_rx, dma_usart2_tx,
+	dma_lpuart1_rx, dma_lpuart1_tx;
 
 DMAPeripheral* struct_DMA_ADC1 = &dma_adc1;
 DMAPeripheral* struct_DMA_ADC2 = &dma_adc2;
@@ -25,51 +27,126 @@ DMAPeripheral* struct_DMA_SPI2_TX = &dma_spi2_tx;
 DMAPeripheral* struct_DMA_SPI3_RX = &dma_spi3_rx;
 DMAPeripheral* struct_DMA_SPI3_TX = &dma_spi3_tx;
 
+DMAPeripheral* struct_DMA_USART1_RX = &dma_usart1_rx;
+DMAPeripheral* struct_DMA_USART1_TX = &dma_usart1_tx;
+
+DMAPeripheral* struct_DMA_USART2_RX = &dma_usart2_rx;
+DMAPeripheral* struct_DMA_USART2_TX = &dma_usart2_tx;
+
+DMAPeripheral* struct_DMA_LPUART1_RX = &dma_lpuart1_rx;
+DMAPeripheral* struct_DMA_LPUART1_TX = &dma_lpuart1_tx;
+
+//Arguably it's better to expand the CSELR information when needed
+//I argue it's easier to understand and debug (always see the info)
+void DMA_initWrapper(DMAPeripheral* peripheral, DMA_Channel_TypeDef* channel, uint8_t cs) {
+	peripheral->channel = channel;
+	peripheral->channel_select_value = cs;
+
+	if ((uint32_t)channel < (uint32_t)DMA2_Channel1_BASE) { //DMA1
+		peripheral->channel_select = DMA1_CSELR;
+		peripheral->channel_select_pos = DMA_CSELR_C1S_Pos;
+	} else { //DMA2
+		peripheral->channel_select = DMA2_CSELR;
+		peripheral->channel_select_pos = DMA_CSELR_C2S_Pos;
+	}
+}
+
+
 void DMA_initializePeripheralConstants() {
-	struct_DMA_ADC1->channel = DMA1_Channel1;
-	struct_DMA_ADC1->channel_select = DMA1_CSELR;
-	struct_DMA_ADC1->channel_select_pos = DMA_CSELR_C1S_Pos;
-	struct_DMA_ADC1->channel_select_value = 0b0;
+	DMA_initWrapper(
+		struct_DMA_ADC1,
+		DMA1_Channel1,
+		0b0
+	);
 
-	struct_DMA_ADC2->channel = DMA1_Channel2;
-	struct_DMA_ADC2->channel_select = DMA1_CSELR;
-	struct_DMA_ADC2->channel_select_pos = DMA_CSELR_C2S_Pos;
-	struct_DMA_ADC2->channel_select_value = 0b0;
+	DMA_initWrapper(
+		struct_DMA_ADC2,
+		DMA1_Channel2,
+		0b0
+	);
 
-	struct_DMA_ADC3->channel = DMA1_Channel3;
-	struct_DMA_ADC3->channel_select = DMA1_CSELR;
-	struct_DMA_ADC3->channel_select_pos = DMA_CSELR_C3S_Pos;
-	struct_DMA_ADC3->channel_select_value = 0b0;
+	DMA_initWrapper(
+		struct_DMA_ADC3,
+		DMA1_Channel3,
+		0b0
+	);
 
-	struct_DMA_SPI1_RX->channel = DMA1_Channel2;
-	struct_DMA_SPI1_RX->channel_select = DMA1_CSELR;
-	struct_DMA_SPI1_RX->channel_select_pos = DMA_CSELR_C2S_Pos;
-	struct_DMA_SPI1_RX->channel_select_value = 0b0001;
+	DMA_initWrapper(
+		struct_DMA_SPI1_RX,
+		DMA1_Channel2,
+		0b0001
+	);
+	DMA_initWrapper(
+		struct_DMA_SPI1_TX,
+		DMA1_Channel3,
+		0b0001
+	);
 
-	struct_DMA_SPI1_TX->channel = DMA1_Channel3;
-	struct_DMA_SPI1_TX->channel_select = DMA1_CSELR;
-	struct_DMA_SPI1_TX->channel_select_pos = DMA_CSELR_C3S_Pos;
-	struct_DMA_SPI1_TX->channel_select_value = 0b0001;
+	DMA_initWrapper(
+		struct_DMA_SPI2_RX,
+		DMA1_Channel4,
+		0b0001
+	);
+	DMA_initWrapper(
+		struct_DMA_SPI2_TX,
+		DMA1_Channel5,
+		0b0001
+	);
 
-	struct_DMA_SPI2_RX->channel = DMA1_Channel4;
-	struct_DMA_SPI2_RX->channel_select = DMA1_CSELR;
-	struct_DMA_SPI2_RX->channel_select_pos = DMA_CSELR_C4S_Pos;
-	struct_DMA_SPI2_RX->channel_select_value = 0b0001;
+	DMA_initWrapper(
+		struct_DMA_SPI3_RX,
+		DMA2_Channel1,
+		0b0011
+	);
+	DMA_initWrapper(
+		struct_DMA_SPI3_TX,
+		DMA2_Channel2,
+		0b0011
+	);
 
-	struct_DMA_SPI2_TX->channel = DMA1_Channel5;
-	struct_DMA_SPI2_TX->channel_select = DMA1_CSELR;
-	struct_DMA_SPI2_TX->channel_select_pos = DMA_CSELR_C5S_Pos;
-	struct_DMA_SPI2_TX->channel_select_value = 0b0001;
+	DMA_initWrapper(
+		struct_DMA_SPI3_TX,
+		DMA2_Channel2,
+		0b0011
+	);
+	DMA_initWrapper(
+		struct_DMA_SPI3_TX,
+		DMA2_Channel2,
+		0b0011
+	);
 
-	struct_DMA_SPI3_RX->channel = DMA2_Channel1;
-	struct_DMA_SPI3_RX->channel_select = DMA2_CSELR;
-	struct_DMA_SPI3_RX->channel_select_pos = DMA_CSELR_C1S_Pos;
-	struct_DMA_SPI3_RX->channel_select_value = 0b0011;
+	DMA_initWrapper(
+		struct_DMA_USART1_RX,
+		DMA1_Channel5,
+		0b0010
+	);
+	DMA_initWrapper(
+		struct_DMA_USART1_TX,
+		DMA1_Channel4,
+		0b0010
+	);
 
-	struct_DMA_SPI3_TX->channel = DMA2_Channel2;
-	struct_DMA_SPI3_TX->channel_select = DMA2_CSELR;
-	struct_DMA_SPI3_TX->channel_select_pos = DMA_CSELR_C2S_Pos;
-	struct_DMA_SPI3_TX->channel_select_value = 0b0011;
+	DMA_initWrapper(
+		struct_DMA_USART2_RX,
+		DMA1_Channel6,
+		0b0010
+	);
+	DMA_initWrapper(
+		struct_DMA_USART2_TX,
+		DMA1_Channel7,
+		0b0010
+	);
+
+	DMA_initWrapper(
+		struct_DMA_LPUART1_RX,
+		DMA2_Channel7,
+		0b0010
+	);
+	DMA_initWrapper(
+		struct_DMA_LPUART1_TX,
+		DMA2_Channel6,
+		0b0010
+	);
 }
 
 DMAPeripheral* DMA_selectPeripheral(enum_DMAPeripherals selection) {
