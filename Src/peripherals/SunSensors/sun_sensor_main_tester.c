82b4f9832dcc0a41c802635e012eebc0083732be
Paige Phung <paigephung15@gmail.com>
2025-05-30 11:01:56 -0700

    Updated sun vector algorithm

--- Src/peripherals/SunSensors/sun_sensor_main_tester.c
+++ Src/peripherals/SunSensors/sun_sensor_main_tester.c
@@ -1,43 +1,67 @@
-#include <SunSensors/sun_sensors.h>
+#include "SunSensors/sun_sensors.h"
 #include <globals.h>
 #include <print_scan.h>
 #include <Buttons/buttons.h>
+#include <math.h>
 
+#define V0 0.05
 
+const float normal_vectors[6][3] = {
+    { 1.0f,  0.0f,  0.0f}, 
+    {-1.0f,  0.0f,  0.0f},
+    { 0.0f,  1.0f,  0.0f}, 
+    { 0.0f, -1.0f,  0.0f}, 
+    { 0.0f,  0.0f,  1.0f}, 
+    { 0.0f,  0.0f, -1.0f}  
+};
+
+void normalize_readings(float voltages[6])
+{
+  for(int i = 0; i < 6; i++)
+  {
+    voltages[i] = voltages[i]/V0;
+  }
+}
+
+void calculate_sun_vector(const float voltages[6], float sun_vector[3])
+{
+  float vx = (voltages[0] > voltages[1]) ? voltages[0] : -voltages[1];
+  float vy = (voltages[2] > voltages[3]) ? voltages[2] : -voltages[3];
+  float vz = (voltages[4] > voltages[5]) ? voltages[4] : -voltages[5];
+
+  sun_vector[0] = vx;
+  sun_vector[1] = vy;
+  sun_vector[2] = vz;
+
+  float sum_of_squares = vx * vx + vy * vy + vz * vz;
+  float mag = sqrtf(sum_of_squares);
+
+  for(int i = 0; i < 3; i ++)
+  {    
+    if (mag > 1e-6) {
+        sun_vector[i] = sun_vector[i]/mag;
+    } else {
+        sun_vector[i] = 0.0f;
+    }
+  }
+}
 
 void testFunction_Sun_Sensor_Main(){
-    float n1[3] = {1.0, 1.0, 1.0};
-    float n2[3] = {1.0, 1.0, 1.0};
-    float n3[3] = {1.0, 1.0, 1.0};
 
-    int readings[6] = {0,0,0,0,0,0};
+    float readings[6] = {0,0,0,0,0,0};
 
-    float vs[3] = {0.0,0.0,0.0};
-	while (1) {
-		if (get_buttonStatus_SW1()) {
+    float sun_vector[3] = {0.0,0.0,0.0};
+    while (1) {
+        if (get_buttonStatus_SW1()) {
             //for testing purposes: this will get readings from each diode on 3 panels
             //prints for panel 0 (side face)
-            printMsg("Reading Voltage\r\n");
-			float voltage00 = sun_sensors_readVoltage(PANEL0, DIODE0);
-            printMsg("Voltage 1: %f\r\n", voltage00);
-            float voltage01 = sun_sensors_readVoltage(PANEL0, DIODE1);
-            printMsg("Voltage 2: %f\r\n", voltage01);
+            readings[0] = sun_sensors_readVoltage(PANEL0, DIODE0);
+            printMsg("Voltage 1: %f\r\n", readings[0]);
+            readings[1] = sun_sensors_readVoltage(PANEL0, DIODE1);
+            printMsg("Voltage 2: %f\r\n", readings[1]);
 
-            //prints voltage readings for panel 1 (side face)
-            printMsg("Reading Voltage\r\n");
-			float voltage10 = sun_sensors_readVoltage(PANEL1, DIODE0);
-            printMsg("Voltage 1: %f\r\n", voltage10);
-            float voltage11 = sun_sensors_readVoltage(PANEL1, DIODE1);
-            printMsg("Voltage 2: %f\r\n", voltage11);
-            
-            //prints voltage readings for panel 4 (top face)
-            printMsg("Reading Voltage\r\n");
-			float voltage40 = sun_sensors_readVoltage(PANEL4, DIODE0);
-            printMsg("Voltage 1: %f\r\n", voltage40);
-            float voltage41 = sun_sensors_readVoltage(PANEL4, DIODE1);
-            printMsg("Voltage 2: %f\r\n", voltage41);
-		}
-	}
-    
-
+            calculate_sun_vector(readings,sun_vector);
+            printMsg("Predicted sun vector: [%f, %f, %f]\r\n", sun_vector[0], sun_vector[1], sun_vector[2]);
+        }
+    }
 }
