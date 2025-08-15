8ac5f553acfd5f91290fef663ad4cf7e8da2d2b7
HistidineDwarf <dvorakdwarf@gmail.com>
2025-08-15 16:51:59 -0700

    Watchdog resets when it should and doesn't when we do sleep cycling

--- Src/system_config/WDG/watchdog_test.c
+++ Src/system_config/WDG/watchdog_test.c
@@ -4,11 +4,16 @@
 
 void testFunction_watchdog() {
     printMsg("start watchdog\n");
-    led_d2(true);
-    watchdog_config(420);
     int count = 0;
     while(1){
-        delay_ms(5000);
+        delay_ms(1000);
+        count ++;
+        if(count  == 3){
+            watchdog_changeIWDGTimeout(5000);
+        }
+        if(count < 15){
+            continue;
+        }
         printMsg("uh oh\n");
         int x = sun_sensors_readVoltage(PANEL0,DIODE0);
         printMsg("voltage: %f\n", x);
