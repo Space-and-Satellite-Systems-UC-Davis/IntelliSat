68b8c3a310f7c7ab892316fccbfb03f74c2ea4d7
nand1b <182454553+nand1b@users.noreply.github.com>
2025-08-24 11:30:01 -0700

    Add sun sensors code

--- Src/sun_sensors/ss_test.c
+++ Src/sun_sensors/ss_test.c
@@ -1,11 +1,26 @@
 /*
- * 	TestID: 9
- *  Date: 2025-??-??
- *  Author:
+ * TestID: 9
+ *  Date: 2025-08-23
+ *  Author: Soren Keck
  *  Notes:
- *
+ *       This code contains a basic function for reading the value of the photodiode plugged into Sol-0
+ *       when the button is pressed
  */
 
-void testFunction_SS() {
+#include <SunSensors/sun_sensors.h>
+#include <globals.h>
+#include <print_scan.h>
+#include <Buttons/buttons.h>
 
+
+void testFunction_SS(){
+
+    while (1) {
+        if (get_buttonStatus_SW1()) {
+            float d0 = sun_sensors_readVoltage(PANEL0, DIODE0);
+            printMsg("Voltage 1: %f\r\n", d0);
+            float d1 = sun_sensors_readVoltage(PANEL0, DIODE1);
+            printMsg("Voltage 2: %f\r\n", d1);
+        }
+    }
 }
