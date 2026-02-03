d728f6189b48ade9c4137c3af3a4ab8868be2bb1
HistidineDwarf <dvorakdwarf@gmail.com>
2026-02-03 13:05:16 -0800

    Fixed broken mgtintercomm test function names

--- Src/main.c
+++ Src/main.c
@@ -1,7 +1,7 @@
 #include "platform_init.h"
 
-#define RUN_TEST	0	// 0 = run IntelliSat, 1 = run a very specific test
-#define TEST_ID 	0	// ID of the test to run in case RUN_TEST = 1
+#define RUN_TEST	1	// 0 = run IntelliSat, 1 = run a very specific test
+#define TEST_ID 	16	// ID of the test to run in case RUN_TEST = 1
 
 #include <TestDefinition.h>
 
