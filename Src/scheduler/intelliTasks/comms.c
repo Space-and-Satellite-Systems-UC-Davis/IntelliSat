2284ad4130e1076e52757d2f9aee5f322bd43d71
HistidineDwarf <dvorakdwarf@gmail.com>
2026-04-30 15:25:23 -0700

    Remove schedulers

--- Src/scheduler/intelliTasks/comms.c
+++
@@ -1,8 +0,0 @@
-#include "intelliTasks_proto.h"
-
-bool comms_time() {
-    return false;
-}
-void config_comms() {}
-void comms() {}
-void clean_comms() {}
--- Src/scheduler/intelliTasks/experiment.c
+++
@@ -1,8 +0,0 @@
-#include "intelliTasks_proto.h"
-
-bool experiment_time() {
-    return false;
-}
-void config_experiment() {}
-void experiment() {}
-void clean_experiment() {}
