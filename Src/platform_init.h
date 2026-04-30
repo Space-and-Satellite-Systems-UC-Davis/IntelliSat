ea8288806b244c1a98adfa5ce14dc4b591bd22e8
HistidineDwarf <dvorakdwarf@gmail.com>
2026-04-30 15:51:18 -0700

    Return platform_init.h

--- Src/platform_init.h
+++ Src/platform_init.h
@@ -26,6 +26,7 @@
 #include <UART/uart.h>
 #include <IMU/ASM330LHH.h>
 #include <MAG/QMC5883L.h>
+#include <DMA/DMA.h>
 #include <print_scan.h>
 #include <SunSensors/sun_sensors.h>
 #include <WDG/watchdog.h>
@@ -71,15 +72,18 @@ void init_first_time() {
 /**
  * Configures the system's various features,
  * such as clocks, protocol hardware, and more.
+ *
+ * @param run_scheduler If set to true, IntelliSat Scheduler will be active
+ *        in the background
  * @returns None
  */
-void init_platform() {
+void init_platform(bool run_scheduler) {
 
     SCB->CPACR |= (SCB_CPACR_CPN_FULL_ACCESS << SCB_CPACR_CP10_POS
     | SCB_CPACR_CPN_FULL_ACCESS << SCB_CPACR_CP11_POS); // Enable the Floating-Point Unit for full access
     debug_init();
     set_IMU(IMU0);
-	  imu_init(IMU_ODR_3333_Hz, IMU_FS_2_g, IMU_ODR_3333_Hz, IMU_FS_1000_dps);
+    imu_init(IMU_ODR_3333_Hz, IMU_FS_2_g, IMU_ODR_3333_Hz, IMU_FS_1000_dps);
     set_IMU(IMU1);
     imu_init(IMU_ODR_3333_Hz, IMU_FS_8_g, IMU_ODR_3333_Hz, IMU_FS_500_dps);
 
@@ -93,12 +97,12 @@ void init_platform() {
     //Activate GPIO G. From errata. Strange bug-fix.
 	PWR->CR2 |= PWR_CR2_IOSV;
 
-    // systick_init(true);
 	printer_init();
 	led_init();
 	buttons_init();
-    watchdog_init(WWDG_TIMEOUT_TIME);
-    heartbeat_timer_init();
+  dma_initializePeripheralConstants();
+  watchdog_init(WWDG_TIMEOUT_TIME);
+  heartbeat_timer_init();
 }
 
 #endif // REALOP1_PLATFORM_INIT_H
