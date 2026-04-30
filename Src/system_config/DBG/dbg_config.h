3918494c10211f335b338ba2e3652f8af2b1e835
HistidineDwarf <dvorakdwarf@gmail.com>
2026-04-30 16:03:58 -0700

    Delete Src/system_config/DBG/dbg_config.h

--- Src/system_config/DBG/dbg_config.h
+++
@@ -1,9 +0,0 @@
-#include "globals.h"
-
-/**
- * @brief Place your debug configiurations in here
- * Comment out anything that you don't use.
- * To add more read the DBG section in the datasheet.
- * Intended for internal use.
- */
-void debug_init();
