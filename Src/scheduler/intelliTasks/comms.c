86aed6cfc8191d16d6fa3f43d21d6ce2de87ceee
Nithin Senthil <nithin020304@gmail.com>
2026-05-21 19:55:56 -0700

    feat: add template impl for comms RTOS task

--- Src/scheduler/intelliTasks/comms.c
+++ Src/scheduler/intelliTasks/comms.c
@@ -1,8 +1,49 @@
 #include "intelliTasks_proto.h"
+#include "Radio/Radio_intercom.h"
 
-bool comms_time() {
-    return false;
+/**
+ * FreeRTOS task for uplink/downlink
+ * 
+ * @param args  Args passed to the RTOS task handle
+ * 
+ * @returns void
+ */
+static void comms(void *args) {
+
+    // Receive uplink
+    uint8_t chunk[MAX_PAYLOAD_BYTES];
+    radio_force_pull(chunk);
+    RadioPacket* data = (RadioPacket*) chunk;
+
+    // If killall stop immediately
+    if (data->datatype == 'K') { // Condition to check if uplink is kill command
+        radio_killall();
+    }
+
+    // Optionally log uplink here
+    
+    // Loop load radio with downlink data
+    uint8_t downlinkData[MAX_PAYLOAD_BYTES];
+    while (false) {
+        // TODO: Replace condition with check for additional data to pull
+        
+        size_t nChunks = 0;
+        // Load downlinkData with logs to downlink
+        // Some storage retrieval call passing downlinkData
+        // Make sure it returns the number of bytes
+        // getLogFromStorage(downlinkData, &nChunks);
+
+        if (!radio_downlink(downlinkData, nChunks)) {
+            break;
+        }
+    }
+
+    // Force downlink after radio transmit buffer is loaded
+    if (radio_push(NULL, 0)) {
+        // Case: Transmit success
+
+    } else {
+        // Case: Transmit failure
+
+    }
 }
-void config_comms() {}
-void comms() {}
-void clean_comms() {}
