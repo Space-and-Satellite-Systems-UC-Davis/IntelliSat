38e09f16cad8554ee0410f9fa9b88a88a42d4dfa
HistidineDwarf <dvorakdwarf@gmail.com>
2026-08-05 21:23:46 -0700

    Copying old uart and crc seems to have fixed whatever goes wrong in the merge. I'll see a diff check

--- Src/inc/TestDefinition.h
+++ Src/inc/TestDefinition.h
@@ -74,8 +74,8 @@ void (*getTestFunction(int test_id))(void) {
 			return testFunction_RadioSleep;
  		case 29:
 			return testFunction_DMA_USART;
-    case 30:    
-      return testFunction_LogDownlink;
+		case 30:
+			return testFunction_LogDownlink;
 		default:
 			return testFunction_Nothing;
 	}
